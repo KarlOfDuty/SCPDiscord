7af6cde8aeb6ac2f5b36f727cf575bf1c0f495c7
Alessandro Casaburi <giochialessandro35@gmail.com>
2023-10-30 13:31:49 +0100

    Rewrite all commands

--- SCPDiscordPlugin/Commands/GrantReservedSlotCommand.cs
+++ SCPDiscordPlugin/Commands/GrantReservedSlotCommand.cs
@@ -1,66 +1,64 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-using CommandSystem;
-using PluginAPI.Core;
-
 namespace SCPDiscord.Commands
 {
-	[CommandHandler(typeof(GameConsoleCommandHandler))]
-	public class GrantReservedSlotCommand : ICommand
-	{
-		public string Command => "scpdiscord_grantreservedslot";
-		public string[] Aliases => new[] { "scpd_grantreservedslot", "scpd_grs" };
-		public string Description => "Adds a user to the reserved slots list and reloads it.";
-		public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
-		{
-			/*
-			if (sender is Player admin)
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using CommandSystem;
+    //using PluginAPI.Core;
+
+    public class GrantReservedSlotCommand : ICommand
+    {
+        public string Command { get; } = "grantreservedslot";
+        public string[] Aliases { get; } = new[] { "grs" };
+        public string Description { get; } = "Adds a user to the reserved slots list and reloads it.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            /*if (sender is Player admin)
 			{
 				if (!messages.HasPermission("scpdiscord.grantreservedslot"))
 				{
 					return new[] { "You don't have permission to use that command." };
 				}
-			}
-			*/
+			}*/
 
-			if (arguments.Count < 1)
-			{
-				response = "Invalid arguments.";
-				return false;
-			}
+            if (arguments.Count != 1)
+            {
+                response = "Invalid arguments.";
+                return false;
+            }
 
-			string steamID = arguments.At(0).Trim();
-			if (!steamID.EndsWith("@steam") && long.TryParse(steamID, out _))
-			{
-				steamID += "@steam";
-			}
+            string steamID = arguments.Array[2].Trim();
+            if (!steamID.EndsWith("@steam") && long.TryParse(steamID, out _))
+            {
+                steamID += "@steam";
+            }
 
-			if (!Regex.IsMatch(steamID, "[0-9]+@steam"))
-			{
-				response = "Invalid Steam ID provided!";
-				return false;
-			}
+            if (!Regex.IsMatch(steamID, "[0-9]+@steam"))
+            {
+                response = "Invalid Steam ID provided!";
+                return false;
+            }
 
-			string[] reservedSlotsFileRows = File.ReadAllLines(Config.GetReservedSlotPath());
-			if (reservedSlotsFileRows.Any(row => row.Trim().StartsWith(steamID)))
-			{
-				response = "User already has a reserved slot!";
-				return false;
-			}
+            string[] reservedSlotsFileRows = File.ReadAllLines(Config.GetReservedSlotPath());
+            if (reservedSlotsFileRows.Any(row => row.Trim().StartsWith(steamID)))
+            {
+                response = "User already has a reserved slot!";
+                return false;
+            }
 
-			if (arguments.Count > 1) // Add with comment
-			{
-				File.AppendAllLines(Config.GetReservedSlotPath(), new[] { "# SCPDiscord: " + string.Join(" ", arguments.Skip(1)), steamID });
-			}
-			else // Add without comment
-			{
-				File.AppendAllLines(Config.GetReservedSlotPath(), new[] { steamID });
-			}
-			ReservedSlot.Reload();
-			response = "Reserved slot added.";
-			return true;
-		}
-	}
+            if (arguments.Count > 1) // Add with comment
+            {
+                File.AppendAllLines(Config.GetReservedSlotPath(), new[] { "# SCPDiscord: " + string.Join(" ", arguments.Skip(1)), steamID });
+            }
+            else // Add without comment
+            {
+                File.AppendAllLines(Config.GetReservedSlotPath(), new[] { steamID });
+            }
+            ReservedSlot.Reload();
+            response = "Reserved slot added.";
+            return true;
+        }
+    }
 }
--- SCPDiscordPlugin/Commands/VerboseCommand.cs
+++ SCPDiscordPlugin/Commands/VerboseCommand.cs
@@ -1,20 +1,20 @@
-using System;
-using CommandSystem;
-using PluginAPI.Core;
-
 namespace SCPDiscord.Commands
 {
-	[CommandHandler(typeof(GameConsoleCommandHandler))]
-	public class VerboseCommand : ICommand
-	{
-		public string Command => "scpdiscord_verbose";
-		public string[] Aliases => new string[] { "scpd_verbose" };
-		public string Description => "Toggles verbose messages.";
-		public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
-		{
-			Config.SetBool("settings.verbose", !Config.GetBool("settings.verbose"));
-			response = "Verbose messages: " + Config.GetBool("settings.verbose");
-			return true;
-		}
-	}
+    using System;
+    using CommandSystem;
+    //using PluginAPI.Core;
+
+    public class VerboseCommand : ICommand
+    {
+        public string Command { get; } = "verbose";
+        public string[] Aliases { get; } = new string[] { "" };
+        public string Description { get; } = "Toggles verbose messages.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Config.SetBool("settings.verbose", !Config.GetBool("settings.verbose"));
+            response = "Verbose messages: " + Config.GetBool("settings.verbose");
+            return true;
+        }
+    }
 }
