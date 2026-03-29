1a85e0c1f0b44546a29689ecc233f8d88877bf14
Karl Essinger <xkaess22@gmail.com>
2026-03-29 16:57:22 +0200

    Tabs -> Spaces

--- SCPDiscordPlugin/BotCommands/AdminChatCommand.cs
+++ SCPDiscordPlugin/BotCommands/AdminChatCommand.cs
@@ -5,39 +5,39 @@ using SCPDiscordPlugin.Helpers;
 
 namespace SCPDiscord.BotCommands
 {
-	public static class AdminChatCommand
-	{
-		public static void Execute(Interface.AdminChatCommand command)
-		{
-			EmbedMessage embed = new EmbedMessage
-			{
-				Colour = EmbedMessage.Types.DiscordColour.Green,
-				ChannelID = command.ChannelID,
-				InteractionID = command.InteractionID
-			};
+  public static class AdminChatCommand
+  {
+    public static void Execute(Interface.AdminChatCommand command)
+    {
+      EmbedMessage embed = new EmbedMessage
+      {
+        Colour = EmbedMessage.Types.DiscordColour.Green,
+        ChannelID = command.ChannelID,
+        InteractionID = command.InteractionID
+      };
 
-			foreach (var staff in ReferenceHub.AllHubs)
-			{
-				if (staff.isLocalPlayer ||
-				    !PermissionsHandler.IsPermitted(staff.serverRoles.Permissions, PlayerPermissions.AdminChat))
-				{
-					continue;
-				}
-				string message = $"<color=white>{command.DiscordUsername}: {Misc.SanitizeRichText(command.Message.Replace("\n", string.Empty).Replace("\r", string.Empty), "＜", "＞")}</color>";
-				Broadcast.Singleton.TargetAddElement(staff.connectionToClient, message, 5, Broadcast.BroadcastFlags.AdminChat);
-				/**
-				* 0 - Host.NetworkId
-				* ! - Separator
-				* @@ - Make message silent without red background
-				*/
-				//Server.SendAdminChatMessage
-				staff.encryptedChannelManager.TrySendMessageToClient($"0![DISCORD] {message}", EncryptedChannelManager.EncryptedChannel.AdminChat);
-			}
+      foreach (var staff in ReferenceHub.AllHubs)
+      {
+        if (staff.isLocalPlayer ||
+            !PermissionsHandler.IsPermitted(staff.serverRoles.Permissions, PlayerPermissions.AdminChat))
+        {
+          continue;
+        }
+        string message = $"<color=white>{command.DiscordUsername}: {Misc.SanitizeRichText(command.Message.Replace("\n", string.Empty).Replace("\r", string.Empty), "＜", "＞")}</color>";
+        Broadcast.Singleton.TargetAddElement(staff.connectionToClient, message, 5, Broadcast.BroadcastFlags.AdminChat);
+        /**
+        * 0 - Host.NetworkId
+        * ! - Separator
+        * @@ - Make message silent without red background
+        */
+        //Server.SendAdminChatMessage
+        staff.encryptedChannelManager.TrySendMessageToClient($"0![DISCORD] {message}", EncryptedChannelManager.EncryptedChannel.AdminChat);
+      }
 
-			// Invoke event to trigger that admin chat message was sent
-			ServerEvents.OnSentAdminChat(new SentAdminChatEventArgs(new DiscordCommandSender(command.DiscordUserID, command.DiscordUsername), command.Message));
+      // Invoke event to trigger that admin chat message was sent
+      ServerEvents.OnSentAdminChat(new SentAdminChatEventArgs(new DiscordCommandSender(command.DiscordUserID, command.DiscordUsername), command.Message));
 
-			SCPDiscord.SendEmbedWithMessageByID(embed, "messages.adminchatsent");
-		}
-	}
+      SCPDiscord.SendEmbedWithMessageByID(embed, "messages.adminchatsent");
+    }
+  }
 }
